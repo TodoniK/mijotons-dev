f05a6cc81fd58028d6cee15f347f14c1627ccc26
Hurrez <abdelhak.yanispro@gmail.com>
2022-03-08 22:04:16 +0100

    Mise en place vu detail recette

---
+++ app/src/main/java/com/example/mijotons/detail_recette.java
@@ -0,0 +1,14 @@
+package com.example.mijotons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detail_recette extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recette);
+    }
+}
